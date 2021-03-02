896d81b4628bc9a6eda9a966a8fa448ea0a06e17
Vincent_Lan <lanfuli007@gmail.com>
2021-03-01 22:36:47 -0800

    [ADD] Add Dashboard Column
    Delete has some issue in the column

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,78 +1,92 @@
+# pylint: disable=no-member
+# pylint: disable=broad-except
+
+"""Website views"""
+import json
+import requests
 from django.shortcuts import render, redirect
-from .models import Stock
-from django.core.exceptions import ObjectDoesNotExist
-from .forms import StockForm
 from django.contrib import messages
+# from stocks.errors import InvalidFormError
+from .models import Stock
+from .forms import StockForm
 import sys
-sys.path.insert(1, '/Users/vincentlan/Documents/GitHub/Portfolio-Management/')
+
+# sys.path.insert(1, '/Users/vincentlan/Documents/GitHub/Portfolio-Management-branch2/')
 from scraping.scraper import Scraper
 from django import template
+
 register = template.Library()
+
 
 # Create your views here.
 def home_view(request):
-    import requests
-    import json
-    
+    """The home view"""
+
     if request.method == "POST":
         ticker = request.POST['ticker']
-        print(type(ticker))
         scraper = Scraper(ticker)
         data = scraper.scrape_all_data()
-        # print(data)
-        # api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_73eb0d679f5f4f73b7d17b90e50923af")
-        # print(api_request.content)
+        # api_request = requests.get(
+        #     "https://cloud.iexapis.com/stable/stock/"
+        #     + ticker
+        #     + "/quote?token=pk_73eb0d679f5f4f73b7d17b90e50923af"
+        # )
+
         try:
-            financials_data = data.get('financials')          
-        except Exception as e:
-            # api = "Error..."
+            financials_data = data.get('financials')
+        except Exception:
             financials_data = "Error..."
         return render(request, 'home.html', {'financials_data': financials_data})
-    else:
-        return render(request, 'home.html', {'ticker': "Enter a ticker symbol above"})
+    return render(
+        request,
+        'home.html',
+        {
+            'ticker': "Enter a ticker symbol above"
+        }
+    )
 
-    return render(request, 'home.html', {'financials_data' : financials_data})
 
 def about_view(request):
+    """The about view"""
     return render(request, 'about.html', {})
 
+
 def dashboard_view(request):
-    import requests
-    import json
+    """The dashboard view"""
     if request.method == "POST":
         form = StockForm(request.POST or None)
-        print(form)
+
         if form.is_valid():
             form.save()
             messages.success(request, "Stock has been added!")
             return redirect('dashboard')
-        else:
-            return redirect('dashboard')
-    else:
-        ticker = Stock.objects.all()
-        output = []
-        # print(ticker)
-        for ticker_item in ticker:
-            # print(str(ticker_item))
-            scraper = Scraper(str(ticker_item))
-            # data = scraper.scrape_all_data()
-            api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+ str(ticker_item) + "/quote?token=pk_73eb0d679f5f4f73b7d17b90e50923af")
-        
-            try:
-                # financials_data = data.get('financials')
-                # output.append(financials_data)
-                api = json.loads(api_request.content)
-                output.append(api)
-            except Exception as e:
-                financials_data = "Error..."
-        # return render(request, 'home.html', {'api' : api })
-        
-        # mylist = zip(output, ticker)
-        # return render(request, 'add_stock.html', {'mylist': mylist})    
-        # return render(request, 'dashboard.html', {'ticker': ticker, 'output': output})
-        return render(request, 'dashboard.html', {'ticker': ticker, 'output': output})
+        # raise InvalidFormError()
+    ticker = Stock.objects.all()
+    output = []
+
+    for ticker_item in ticker:
+        scraper = Scraper(str(ticker_item))
+        try:
+            data = scraper.scrape_all_data()
+            financials_data = data.get('financials')
+            output.append(financials_data)
+        except Exception:
+            api = "Error..."
+    # mylist = zip(output, ticker)
+    # context = {
+    #     'mylist': mylist,
+    # }
+    return render(
+        request,
+        'dashboard.html',
+        {'ticker': ticker,
+         'output': output
+         }
+    )
+
 
 def delete(request, stock_id):
+    """The delete view"""
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, "Stock has been delete!")
