fb00aa3af95c92a639fd284713687f7cd26bb04e
Vincent_Lan <lanfuli007@gmail.com>
2021-03-06 20:37:16 -0800

    [Add] Preventing duplicate object input

--- stocks/quotes/models.py
+++ stocks/quotes/models.py
@@ -6,7 +6,10 @@ from django.db import models
 # Create your models here.
 class Stock(models.Model):
     """Stock model"""
-    ticker = models.CharField(max_length=10)
+    ticker = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.ticker
+
+    def clean(self):
+        self.ticker = self.ticker.upper()
