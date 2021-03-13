60034922db087ef7f59400536e6a1db3d1c56028
Aniruddha Dutta <aniruddhadutta29@gmail.com>
2021-03-12 19:32:08 -0800

    Updated urls
    updated urls according to the changes in views.py

--- stocks/quotes/urls.py
+++ stocks/quotes/urls.py
@@ -1,10 +1,13 @@
 """Routing module"""
 from django.urls import path
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
+	path('accounts/', include("django.contrib.auth.urls")),
     path('', views.home_view, name="home"),
     path('about/', views.about_view, name="about"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
-    path("delete/<stock_id>", views.delete, name="delete")
+    path("delete/<stock_id>", views.delete, name="delete"),
+    path("download/", views.download_csv, name="download")
 ]
