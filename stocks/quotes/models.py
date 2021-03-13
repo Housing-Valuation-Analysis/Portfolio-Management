00659722f5de88e1ceb7cee623976db5e40cb462
Aniruddha Dutta <aniruddhadutta29@gmail.com>
2021-03-12 19:33:11 -0800

    Updated to return user input fields
    Updated to return user input fields

--- stocks/quotes/models.py
+++ stocks/quotes/models.py
@@ -1,6 +1,7 @@
 # pylint: disable=invalid-str-returned
 """Models for website"""
 from django.db import models
+
 from datetime import date
 
 
@@ -8,12 +9,18 @@ from datetime import date
 class Stock(models.Model):
     """Stock model"""
     ticker = models.CharField(max_length=10, unique=True)
+
+    
     entry_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True)
     entry_date = models.DateField(default=date.today, blank=True)
-    shares = models.DecimalField(max_digits=10, decimal_places=3, default=0, null=0, blank=True)
+    shares = models.DecimalField(max_digits=10, decimal_places=3, default=4, null=0, blank=True)
+    
 
     def __str__(self):
-        return self.ticker
+        return '{}{}{}{}{}{}{}'.format(self.ticker, ',', self.entry_price, ',', self.entry_date, ',', self.shares)
+        #zip_list = zip(self.ticker, self.entry_price, self.entry_date, self.shares)
+        #return zip_list
+
 
     def clean(self):
         self.ticker = self.ticker.upper()
