e6344f907255b8a42bb5da1d43f1a415c8c39594
Aniruddha Dutta <aniruddhadutta29@gmail.com>
2021-03-14 19:48:51 -0700

    Fixed linting errors
    fixed linting errors

--- stocks/quotes/viewsController.py
+++ stocks/quotes/viewsController.py
@@ -1,11 +1,15 @@
-from scraping.scraper import Scraper
+# pylint: disable=invalid-name, missing-module-docstring, wrong-import-position
+# pylint: disable=missing-function-docstring, import-error
+
+from scraping.scraper import Scraper  # noqa:E402
+
 
 def retrieve_by_ticker(ticker):
     return retrieve_by_scraper(Scraper(ticker))
+
 
 # Separated into two functions for testing purpose
 def retrieve_by_scraper(scraper):
 
     data = scraper.scrape_all_data()
     return data.get('financials')
-
